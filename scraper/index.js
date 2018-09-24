35c3021e516abfeeae0d4db711b0c2f702fb5c77
amirfuhaira <amir@triggernext.com>
2018-09-24 15:43:41 +0800

    retry load page if errored

--- scraper/index.js
+++ scraper/index.js
@@ -1,5 +1,4 @@
 const superagent = require('superagent');
-require('superagent-retry-delay')(superagent);
 const agent = superagent.agent();
 const cheerio = require('cheerio');
 const fs = require('fs');
@@ -18,12 +17,15 @@ let sessionId = '';
 let acpSessionId = '';
 let memberIdCount = 0;
 
-async function getMainIndex() {
+/**
+ * Start scraping.
+ */
+async function start() {
   /**
    * Login to get session ID, and pass cookies to agent.
    */
   
-  const loginPage = cheerio.load((await agent.get(`${forumParams.url}ucp.php?mode=login`)).text);
+  const loginPage = await loadForumPage(`${forumParams.url}ucp.php?mode=login`);
   sessionId = loginPage('input[name="sid"]').val();
 
   log('Logging into forum...');
@@ -36,8 +38,6 @@ async function getMainIndex() {
     .field('username', forumParams.user)
     .field('redirect', `${forumParams.url}index.php`)
     .field('sid', sessionId);
-
-  /* testWriteFile('loginRes.txt', loginRes); */
 
   /**
    * Parse the resulting main page.
@@ -78,7 +78,7 @@ async function getMainIndex() {
       }
     }).get();
   const admLink = mainPage('.dropdown a').first().attr('href');
-  memberIdCount = Number(mainPage('.stat-block.statistics strong').last().children().attr('href').match(/u=(\d+)$/)[1]);
+  memberIdCount = Number(mainPage('.stat-block.statistics a').last().attr('href').match(/u=(\d+)$/)[1]);
 
   log(`Forum: ${forumName} | Description: ${forumDesc} | Member ID Count: ${memberIdCount}`);
   log(`Logged in as User: ${loggedUser} | Avatar ${loggedAvatar} | Session ID: ${sessionId}`);
@@ -89,7 +89,7 @@ async function getMainIndex() {
    * Log into ACP with admin console link.
    */
   log('Logging into ACP...');
-  const acpLoginPage = cheerio.load((await agent.get(admLink)).text);
+  const acpLoginPage = await loadForumPage(admLink);
   const credential = acpLoginPage('input[name="credential"]').val();
   acpSessionId = acpLoginPage('input[name="sid"]').val();
 
@@ -102,8 +102,6 @@ async function getMainIndex() {
     .field('redirect', admLink)
     .field('sid', acpSessionId);
 
-  /* testWriteFile('acpLogin.txt', acpLoginRes); */
-
   const acpMainPage = cheerio.load(acpLoginRes.text);
   const firstRowIP = acpMainPage('.zebra-table td').eq(1).text();
 
@@ -114,7 +112,7 @@ async function getMainIndex() {
    * Member backup.
    */
   const memberIds = Array.from({ length: memberIdCount + 1 }, (_, i) => i);
-  const members = await asyncPool(2, memberIds, getMember);
+  const members = await asyncPool(3, memberIds, getMember);
 
   testWriteFile(`memberData(${new Date().toISOString()}).json`, members);
 
@@ -127,30 +125,35 @@ async function getMainIndex() {
   log(JSON.stringify(forumIndex, null, 2)); */
 }
 
+/**
+ * Get info for a specified member.
+ */
 async function getMember(userId) {
   if (userId === 0) {
     return { id: 0 };
   }
-  const userPage = cheerio.load((await agent.get(`${forumParams.url}memberlist.php?mode=viewprofile&u=${userId}`)).text);
+  const userPage = await loadForumPage(`${forumParams.url}memberlist.php?mode=viewprofile&u=${userId}`);
   if (userPage('.operation a').length) {
     const name = userPage('.profile .username').text().trim();
     log(`Getting ACP user info of user ${name} id ${userId}`);
 
     const userPageAdmUrl = userPage('.operation a').filter(function() { return userPage(this).attr('href').includes('/adm/') }).attr('href');
-    const userPageAdm = cheerio.load((await agent.get(userPageAdmUrl)).text);
+    const userPageAdm = await loadAcpPage(userPageAdmUrl);
     const currentSid = userPageAdmUrl.match(/&sid=(\w+)$/)[1];
-    const userPageAdmSig = cheerio.load((await agent.get(`${forumParams.url}adm/index.php?i=users&mode=sig&u=${userId}&sid=${currentSid}`)).text);
-    const userPageAdmProf = cheerio.load((await agent.get(`${forumParams.url}adm/index.php?i=users&mode=profile&u=${userId}&sid=${currentSid}`)).text);
+    const userPageAdmSig = await loadAcpPage(`${forumParams.url}adm/index.php?i=users&mode=sig&u=${userId}&sid=${currentSid}`);
+    const userPageAdmProf = await loadAcpPage(`${forumParams.url}adm/index.php?i=users&mode=profile&u=${userId}&sid=${currentSid}`);
 
     const userProfile = userPage('.profile .group .clear-after');
+    let userAvatar = userPage('.profile .avatar-bg').css('background-image').slice(4).slice(0, -1);
+    userAvatar = userAvatar === 'https://groups.tapatalk-cdn.com/static/image/no_avatar.png' ? null : userAvatar;
     const user = {
       id: userId,
       name,
-      avatar: userPage('.profile .avatar-bg').css('background-image').slice(4).slice(0, -1),
+      avatar: userAvatar,
       email: userPageAdm('#user_email_search').val(),
       reg: dateConv(userProfile.filter(function() { return userPage(this).text().includes('Joined') }).find('.right').text()),
       sig: userPageAdmSig('textarea[name="signature"]').val(),
-      int: userProfile.filter(function() { return userPage(this).text().includes('Interests') }).length ? userProfile.filter(function() { return userPage(this).text().includes('Interests') }).find('.right').html().replace('<br>', '\n') : "",
+      int: userProfile.filter(function() { return userPage(this).text().includes('Interests') }).length ? userProfile.filter(function() { return userPage(this).text().includes('Interests') }).find('.right').html().replace(/<br>/g, '\n') : "",
       birth: {
         year: parseInt(userPageAdmProf('select[name="bday_year"]').val()),
         month: parseInt(userPageAdmProf('select[name="bday_month"]').val()),
@@ -163,6 +166,71 @@ async function getMember(userId) {
   return { id: userId };
 }
 
+/**
+ * Get info for a specific forum thread.
+ */
+async function getThreadData(threadId) {
+  const firstPage = await loadForumPage(`${forumParams.url}null-t${threadId}.html`);
+}
+
+/**
+ * Page-loading functions.
+ */
+
+async function loadForumPage(pageUrl, limit = 1) {
+  const maxlimit = 5;
+  try {
+    const page = cheerio.load((await agent.get(pageUrl)).text);
+    if (!page('.navbar-forum-name').text()) {
+      if (limit !== maxlimit) {
+        testWriteFile('errPageRes.txt', page);
+        throw new Error(`Error loading page ${pageUrl}, limit exceeded ${maxlimit}. Ending scrape session and writing errored page to disk.`);
+      }
+      log(`Error loading page ${pageUrl}, attempt #${limit}, delaying next attempt by ${2000 * limit}ms`);
+      await sleep(2000 * limit);
+      return loadForumPage(pageUrl, limit + 1);
+    }
+    return page;
+  } catch (err) {
+    if (limit !== maxlimit) {
+      log(`Error loading page ${pageUrl}, limit exceeded ${maxlimit}. Ending scrape session.`);
+      throw err;
+    }
+    log(`Error loading page ${pageUrl}, attempt #${limit}, delaying next attempt by ${2000 * limit}ms`);
+    await sleep(2000 * limit);
+    return loadForumPage(pageUrl, limit + 1);
+  }
+}
+
+async function loadAcpPage(pageUrl, limit = 1) {
+  const maxlimit = 5;
+  try {
+    const page = cheerio.load((await agent.get(pageUrl)).text);
+    if (page('#page-header h1').text() !== 'Administration Control Panel') {
+      if (limit !== maxlimit) {
+        testWriteFile('errPageRes.txt', page);
+        throw new Error(`Error loading page ${pageUrl}, limit exceeded ${maxlimit}. Ending scrape session and writing errored page to disk.`);
+      }
+      log(`Error loading page ${pageUrl}, attempt #${limit}, delaying next attempt by ${2000 * limit}ms`);
+      await sleep(2000 * limit);
+      return loadAcpPage(pageUrl, limit + 1);
+    }
+    return page;
+  } catch (err) {
+    if (limit !== maxlimit) {
+      log(`Error loading page ${pageUrl}, limit exceeded ${maxlimit}. Ending scrape session.`);
+      throw err;
+    }
+    log(err.toString());
+    log(`Error loading page ${pageUrl}, attempt #${limit}, delaying next attempt by ${2000 * limit}ms`);
+    await sleep(2000 * limit);
+    return loadAcpPage(pageUrl, limit + 1);
+  }
+}
+
+/**
+ * Main startup function.
+ */
 async function main() {
   const now = new Date().getTime();
   line();
@@ -171,7 +239,7 @@ async function main() {
   line();
 
   try {
-    await getMainIndex();
+    await start();
   } catch (err) {
     log('Scraper error!');
     line();
@@ -206,7 +274,12 @@ function testWriteFile(filename, data) {
   line();
   log(`Writing to test file ${file}`);
   line();
-  fs.writeFileSync(`./output/${file}`, typeof data === 'string' ? data : JSON.stringify(data, null, 2));
+  const outputDir = './output';
+
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+  fs.writeFileSync(`${outputDir}/${file}`, typeof data === 'string' ? data : JSON.stringify(data, null, 2));
 }
 
 function timeCounter(msecond) {
@@ -229,4 +302,8 @@ function timeCounter(msecond) {
   if (minutes) content.push(minutes + " minute" + (minutes > 1 ? "s" : ""));
   if (t) content.push(t + " second" + (t > 1 ? "s" : ""));
   return content.slice(0,3).join(', ');
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
